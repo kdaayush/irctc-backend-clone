69e5966d4a252aca9ba5b3b6cb02d6e815eb26de
kd45aayush <115484536+kdaayush@users.noreply.github.com>
2025-10-22 12:18:14 +0530

    Add files via upload

---
+++ irctc/app/src/main/java/org/example/entities/user.java
@@ -0,0 +1,8 @@
+package org.example.entities;
+
+public class user {
+    private string name;
+    private string password;
+    private string hashpassword;
+    private List<Ticket> ticketsBooked;
+}
